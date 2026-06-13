afb4f46eb3c31049de056f59c106cafdaf06299c
gjg <gjg@tskoli.is>
2026-06-13 11:22:34 +0000

    v2

--- verkefni-2/app.py
+++ verkefni-2/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from flask_ckeditor import CKEditor
+#from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 
 app.secret_key = 'Þe551_lyki11_Er_3rf1ður!' # Nauðsynlegt fyrir session
 
 # CKEditor
-app.config['CKEDITOR_PKG_TYPE'] = 'basic'
-ckeditor = CKEditor(app)
+#app.config['CKEDITOR_PKG_TYPE'] = 'basic'
+#ckeditor = CKEditor(app)
 
 # Einfaldur "gagnagrunnur" í vinnsluminni (cache) 
 nemendur = {
@@ -15,8 +15,15 @@ nemendur = {
     "2": {"nafn": "Anna Önnudóttir", "netfang": "anna@skoli.is"}
 }
 skilabod = {
-    "1": {"fyrirsogn": "Klúbbastarfið byrjað", "postur": "Þetta eru fyrstu skilaboðin í skjóðunni" },
-    "2": {"fyrirsogn": "Hvað er á dagsskrá? ", "postur": "Nú er þetta allt að smella saman 😉​" }
+    "1": {"fyrirsogn": "Klúbbastarfið byrjað", 
+          "postur": "Þetta eru fyrstu skilaboðin í skjóðunni", 
+          "hofundur": "Jón Jónsson"},
+    "2": {"fyrirsogn": "Hvað er á dagsskrá? ", 
+          "postur": "Nú er þetta allt að smella saman 😉​", 
+          "hofundur": "Anna Önnudóttir"}
+}
+admin = {
+    "1": {"admin": "Addiminn", "netfang": "admin@skoli.is"}
 }
 
 # Read
@@ -25,7 +32,7 @@ skilabod = {
 def index():
     title = "Skilaboðaskjóðan"
     # Birtir alla nemendur úr orðasafninu og skilaboð
-    return render_template('index.html', nemendur=nemendur, skilabod=skilabod, title=title)
+    return render_template('index.html', skilabod=skilabod, title=title)
 
 # finnum nemanda eftir id og sendum á profile.html
 
@@ -35,22 +42,45 @@ def view_student(id):
     nemandi = nemendur.get(id)
     return render_template('profile.html', nemandi=nemandi)
 
-# Create (nemanda í nemendur)
+@app.route('/admin/<id>')
+def view_admin(id):
+    # Sækir admin með lykli (key)
+    admin = admin.get(id)
+    return render_template('admin.html', nemendur=nemendur)
 
+# Create 
+# (Nýskráning nemanda í klúbbinn)
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
-        # Sækir gögn úr formi
-        nytt_id = str(len(nemendur) + 1) #nýtt id búið til + 1
+        # Sækir fjölda nemenda úr nemandalistanum og bætir einum við (+ 1)
+        nytt_id = str(len(nemendur) + 1) #nýtt id búið til 
         nafn = request.form.get('nafn')
         netfang = request.form.get('netfang')
         
-        # Bætir við í orðasafnið
+        # Bætir nemanda í listann (sett í vinnsluminni núna en verður útfært í 3. verkefni)
         nemendur[nytt_id] = {"nafn": nafn, "netfang": netfang}
         return redirect(url_for('index'))
     return render_template('create_form.html')
 
-# Update
+# (Setjum nýjan póst í skilaboðasskjóðuna)
+
+@app.route('/skrifa', methods=['GET', 'POST'])
+def skrifa():
+    if request.method == 'POST':
+        # Sækir fjölda pósta úr skilabod listanum og bætir einum við (+ 1)
+        post_id = str(len(skilabod) + 1) #nýtt post id búið til 
+        fyrirsogn = request.form.get('fyrirsogn')
+        postur = request.form.get('postur')
+        hofundur = request.form.get('hofundur')
+
+        # Bætir nýjum pósti í listann (sett í vinnsluminni núna en verður útfært betur í 3. verkefni)
+        skilabod[post_id] = {"fyrirsogn": fyrirsogn, "postur": postur, "hofundur": hofundur}
+        flash('Ný skilaboð!')
+        return redirect(url_for('index'))
+    return render_template('create_form.html')
+
+# Update sett í verkefni 3
 
 @app.route('/update/<id>', methods=['GET', 'POST'])
 def update(id):
@@ -63,14 +93,16 @@ def update(id):
     nemandi = nemendur.get(id)
     return render_template('update_form.html', nemandi=nemandi, id=id)
 
-# Delete
+# Delete klárað í verkefni 3
 
 @app.route('/delete/<id>')
 def delete(id):
     # Fjarlægir nemanda með gefnu ID
     if id in nemendur:
         nemendur.pop(id)
-    return redirect(url_for('index'))
+    return redirect(url_for('view_admin'))
+
+# login
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -91,6 +123,27 @@ def login():
             
     return render_template('index.html')
 
+# admin login
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        # Sækjum ID úr forminu með 'name' eigindinu
+        admin_id = request.form.get('admin_id')
+        
+        # Athugum hvort lykillinn sé til í nemenda-dictionary
+        if admin_id in admin:
+            # Geymum ID í session svo notandinn haldist innskráður á milli síðna
+            session['admin_id'] = admin_id
+            flash('Velkominn Addi minn!')
+            return redirect(url_for('view_admin'))
+        else:
+            # Ef ID finnst ekki, gefum við endurgjöf
+            flash('Villa: Rangt admin-ID.')
+            return redirect(url_for('index'))
+            
+    return render_template('index.html')
+
 # logout
 
 @app.route('/logout')
@@ -98,31 +151,30 @@ def logout():
     # Fjarlægir user_id úr session ef það er til staðar
     session.pop('user_id', None)
     
-    # Gefa notanda endurgjöf
+    # endurgjöf
     flash('Þú hefur verið skráð(ur) út.')
     
     # Senda notanda aftur á forsíðu eða innskráningarsíðu
     return redirect(url_for('index'))
 
-# Profile
+# Profile - aðeins innskráður nemandi kemst þangað
 
 @app.route('/profile')
 def profile():
-    title = "Prófíllinn þinn"
-    # 1. Athuga hvort notandi sé skráður inn í session
+
+    # 1. Athuga hvort nemandi sé skráður inn í session
     user_id = session.get('user_id')
     
     # 2. Athuga hvort user_id sé í nemendalistanum
     if not user_id or user_id not in nemendur:
         # Ef ekki, senda notanda á forsíðu (aðgangur lokaður)
-        flash('Þú ert ekki skráður í vefáfangann')
+        flash('Þú ert ekki skráður í klúbbinn')
         return redirect(url_for('index'))
     
     # 3. Sækja gögn nemandans og birta síðuna
     nemandi = nemendur[user_id]
     #flash(f'Nemandinn {nemandi} hefur verið skráður!')
     return render_template('profile.html', nemandi=nemandi)
-
 
 # 400 villur
 
